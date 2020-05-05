cecb52dac14d40daf0f3d74682425f19c86c6fc8
wuhaohao1234 <1611499758@qq.com>
2020-05-05 20:54:13 +0800

    添加地址页面

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <van-button color="#FF8B10" size="large">新增收获地址</van-button> 
   </div>
 </template>
+<script>
+export default {
+  name: "About",
+  created() {
+    console.log(1);
+  },
+  methods: {}
+};
+</script>
+<style lang="scss">
+ .van-button {
+   position: fixed;
+   bottom: 0;
+   right: 0;
+ } 
+</style>
---
+++ src/views/AddAddress.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="AddAddress">
+    <van-field v-model="value" placeholder="收货人" />
+    <van-field v-model="value" placeholder="手机号码" />
+
+    <van-field v-model="value" placeholder="所在地区" />
+
+    <van-field v-model="value" type="textarea" placeholder="详细地址" />
+
+    <van-field v-model="value" placeholder="设为默认地址" />
+  </div>
+</template>
+<script>
+export default {
+  name: "AddAddress",
+  created() {
+    console.log(1);
+  },
+  data() {
+    return {
+      value: ""
+    };
+  },
+  methods: {}
+};
+</script>
+<style lang="scss">
+</style>
